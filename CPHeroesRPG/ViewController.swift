d269b2b1dfe9154ca5654af0ff31da13387f4ebb
EGladun <gladunov.e.i.1.17@gmail.com>
2020-03-27 11:42:34 +0300

    Initial Commit

---
+++ CPHeroesRPG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CPHeroesRPG
+//
+//  Created by Egor on 27.03.2020.
+//  Copyright © 2020 Egor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
